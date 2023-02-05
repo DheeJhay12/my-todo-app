eea34f0bb4296c2e5c670aad4de9913696b18b27
DheeJhay12 <32485804+DheeJhay12@users.noreply.github.com>
2023-02-05 23:57:44 +0800

    Initial commit

--- web.py
+++ web.py
@@ -24,9 +24,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Add Todo", placeholder="Add new todo..",
-              on_change=add_todo, key='new_todo')
-
-print("Hello")
 
 st.session_state
